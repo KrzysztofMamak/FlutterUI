aefae5a7a2b738f831550858a22db78f367b08be
KrzysztofMamak <mamako@onet.pl>
2022-08-25 23:43:15 +0200

    Add food data

---
+++ lib/shimmer_effect/food_data.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_samples/shimmer_effect/food.dart';
+
+const foodList = <Food>[
+  Food(
+    title: 'Salad',
+    urlImage:
+        'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+    description: 'Salad description',
+  ),
+  Food(
+    title: 'Fried chicken',
+    urlImage:
+        'https://images.unsplash.com/photo-1562967914-608f82629710?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80',
+    description: 'Fried chicken description',
+  ),
+  Food(
+    title: 'Burger',
+    urlImage:
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1202&q=80',
+    description: 'Burger description',
+  ),
+  Food(
+    title: 'Tacos',
+    urlImage:
+        'https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1371&q=80',
+    description: 'Tacos description',
+  ),
+  Food(
+    title: 'Blueberry pancakes',
+    urlImage:
+        'https://images.unsplash.com/photo-1558401391-7899b4bd5bbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+    description: 'Blueberry pancakes description',
+  ),
+  Food(
+    title: 'Ramen',
+    urlImage:
+        'https://images.unsplash.com/photo-1591814468924-caf88d1232e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    description: 'Ramen description',
+  ),
+  Food(
+    title: 'Pasta',
+    urlImage:
+        'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    description: 'Pasta description',
+  ),
+];
