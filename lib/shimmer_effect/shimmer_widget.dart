68f1be97032c8e866a47b848c5519d60f463092f
KrzysztofMamak <mamako@onet.pl>
2022-08-25 23:43:34 +0200

    Add ShimmerWidget

---
+++ lib/shimmer_effect/shimmer_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerWidget extends StatelessWidget {
+  const ShimmerWidget.rectangular({
+    this.width = double.infinity,
+    required this.height,
+  }) : this.shapeBorder = const RoundedRectangleBorder();
+
+  const ShimmerWidget.circular({
+    required this.width,
+    required this.height,
+    this.shapeBorder = const CircleBorder(),
+  });
+
+  final double width;
+  final double height;
+  final ShapeBorder shapeBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[400]!,
+      highlightColor: Colors.grey[300]!,
+      period: const Duration(seconds: 1),
+      child: Container(
+        width: width,
+        height: height,
+        decoration: ShapeDecoration(
+          color: Colors.grey[400]!,
+          shape: shapeBorder,
+        ),
+      ),
+    );
+  }
+}
