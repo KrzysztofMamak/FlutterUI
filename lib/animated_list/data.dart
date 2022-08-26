01dde8f498dbcaeb3af823bd26dd928e7783fee4
KrzysztofMamak <mamako@onet.pl>
2022-08-27 01:35:02 +0200

    Add list data

---
+++ lib/animated_list/data.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_samples/animated_list/list_item.dart';
+
+const listItems = <ListItem>[
+  ListItem(
+    title: 'Salad',
+    urlImage:
+        'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+  ),
+  ListItem(
+    title: 'Fried chicken',
+    urlImage:
+        'https://images.unsplash.com/photo-1562967914-608f82629710?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80',
+  ),
+  ListItem(
+    title: 'Burger',
+    urlImage:
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1202&q=80',
+  ),
+  ListItem(
+    title: 'Tacos',
+    urlImage:
+        'https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1371&q=80',
+  ),
+  ListItem(
+    title: 'Blueberry pancakes',
+    urlImage:
+        'https://images.unsplash.com/photo-1558401391-7899b4bd5bbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+  ),
+  ListItem(
+    title: 'Ramen',
+    urlImage:
+        'https://images.unsplash.com/photo-1591814468924-caf88d1232e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+];
