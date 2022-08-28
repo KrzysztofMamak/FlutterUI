19f9f2189dd0633683f096bf170bbe430a3f8c75
KrzysztofMamak <mamako@onet.pl>
2022-08-28 21:29:16 +0200

    Add data

---
+++ lib/bouncing_cards/data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_samples/bouncing_cards/card_data.dart';
+
+class Data {
+  static const cards = [
+    CardData(
+      title: 'Minions',
+      description:
+          'Minions is a 2015 American 3D computer-animated comedy film, serving as a spin-off prequel to the Despicable Me franchise.',
+      subtitle: 'Movie',
+      color: Colors.yellow,
+      imgUrl: 'assets/bouncing_cards/minion.jpg',
+    ),
+    CardData(
+      title: 'Robot',
+      description:
+          'A robot is a machine—especially one programmable by a computer— capable of carrying out a complex series of actions automatically.',
+      subtitle: 'A Machine',
+      color: Colors.blue,
+      imgUrl: 'assets/bouncing_cards/robot.jpg',
+    ),
+    CardData(
+      title: 'Angry Birds',
+      description:
+          'Angry Birds is a 2009 casual puzzle video game developed by Rovio Entertainment.',
+      subtitle: 'Movie / Game',
+      color: Colors.red,
+      imgUrl: 'assets/bouncing_cards/angry_birds.jpg',
+    ),
+    CardData(
+      title: 'Minions',
+      description:
+          'Minions is a 2015 American 3D computer-animated comedy film, serving as a spin-off prequel to the Despicable Me franchise.',
+      subtitle: 'Movie',
+      color: Colors.yellow,
+      imgUrl: 'assets/bouncing_cards/minion.jpg',
+    ),
+    CardData(
+      title: 'Robot',
+      description:
+          'A robot is a machine—especially one programmable by a computer— capable of carrying out a complex series of actions automatically. ',
+      subtitle: 'A Machine',
+      color: Colors.blue,
+      imgUrl: 'assets/bouncing_cards/robot.jpg',
+    ),
+    CardData(
+      title: 'Angry Birds',
+      description:
+          'Angry Birds is a 2009 casual puzzle video game developed by Rovio Entertainment. ',
+      subtitle: 'Movie / Game',
+      color: Colors.red,
+      imgUrl: 'assets/bouncing_cards/angry_birds.jpg',
+    ),
+  ];
+}
